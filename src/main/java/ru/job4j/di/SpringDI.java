c273037521c4ffd84d1ab4a3ba1308412e8c8ef1
Maulen Zharylgasynov <exmpl@gmail.com>
2024-09-29 08:13:55 +0600

    Время жизни (Scope)

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -10,9 +10,9 @@ public class SpringDI {
         context.register(ConsoleInput.class);
         context.register(StartUI.class);*/
         context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
-        ui.print();
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
     }
 }
