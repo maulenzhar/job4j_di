a3d357c2e7531e0511a4dca70171ca4810df7ecc
Maulen Zharylgasynov <exmpl@gmail.com>
2024-09-29 07:58:28 +0600

    Сканирование проекта

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -5,9 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
+        context.scan("ru.job4j.di");
+        /*context.register(Store.class);
         context.register(ConsoleInput.class);
-        context.register(StartUI.class);
+        context.register(StartUI.class);*/
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
         ui.add("Petr Arsentev");
